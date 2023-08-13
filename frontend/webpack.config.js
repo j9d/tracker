6bde83d54fc232ed490cf6746492882dae76da62
Jamie Dimos <jamiedimos@gmail.com>
2023-08-13 21:53:55 +1000

    Change frontend to 4 space indent

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,13 +3,13 @@ const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  context: __dirname,
-  entry: './src/index',
-  output: {
-    path: path.resolve('./assets/webpack_bundles/'),
-    filename: "[name]-[contenthash].js"
-  },
-  plugins: [
-    new BundleTracker({filename: './webpack-stats.json'})
-  ],
+    context: __dirname,
+    entry: './src/index',
+    output: {
+        path: path.resolve('./assets/webpack_bundles/'),
+        filename: "[name]-[contenthash].js"
+    },
+    plugins: [
+        new BundleTracker({ filename: './webpack-stats.json' })
+    ],
 }
